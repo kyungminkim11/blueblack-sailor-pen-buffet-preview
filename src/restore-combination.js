d4bd18520f7c6904661d79f43d300068b2e067fa
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-27 09:07:12 +0900

    Use customer-safe session and store experience modules

--- src/restore-combination.js
+++ src/restore-combination.js
@@ -1,22 +1,2 @@
-import './pwa-v5.js';
-import './store-experience-v5.js';
-
-const STORAGE_KEY = 'blueblack-pen-combination-query';
-const PART_IDS = ['cap_body', 'cap_end', 'nib_grip', 'metal_parts', 'barrel_body', 'barrel_end'];
-
-const current = new URLSearchParams(location.search);
-const alreadyHasSelection = PART_IDS.some((id) => current.has(id));
-
-if (!alreadyHasSelection) {
-  const saved = localStorage.getItem(STORAGE_KEY);
-  if (saved) {
-    const savedQuery = new URLSearchParams(saved);
-    for (const id of PART_IDS) {
-      const value = savedQuery.get(id);
-      if (value) current.set(id, value);
-    }
-    const language = savedQuery.get('lang');
-    if (language && !current.has('lang')) current.set('lang', language);
-    history.replaceState(null, '', `${location.pathname}?${current}`);
-  }
-}
+import './pwa-v6.js';
+import './store-experience-v6.js';
