486d4d822a291f36e71e012eac2ecb9127e7a4ee
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-28 04:06:14 +0900

    Seed verified official product image mapping

---
+++ guide/data/product-images.js
@@ -0,0 +1,10 @@
+// Seed mapping. The GitHub Action replaces this file with synchronized local WebP paths.
+export const productImages={
+  'pilot-lightive':{src:'https://blueblack.co.kr/web/product/small/202603/c9f2982c47c145905f34ed4c56a46224.jpg',source:'https://blueblack.co.kr/web/product/small/202603/c9f2982c47c145905f34ed4c56a46224.jpg',alt:'파이롯트 라이티브 만년필'},
+  'lamy-safari':{src:'https://lamysingapore.com/cdn/shop/files/FP-safari-black-2.png?v=1740641341&width=1946',source:'https://lamysingapore.com/cdn/shop/files/FP-safari-black-2.png?v=1740641341&width=1946',alt:'라미 사파리 만년필'},
+  'parker-jotter':{src:'https://blueblack.co.kr/web/product/small/202507/a791dd6177316f2ca43983f5dbc48aac.jpg',source:'https://blueblack.co.kr/web/product/small/202507/a791dd6177316f2ca43983f5dbc48aac.jpg',alt:'파카 조터 오리지널 만년필'},
+  'sailor-tuzu':{src:'https://blueblack.co.kr/web/product/small/202606/85dd131c9b6e792ce2f943bb2c31e0ca.jpg',source:'https://blueblack.co.kr/web/product/small/202606/85dd131c9b6e792ce2f943bb2c31e0ca.jpg',alt:'세일러 투주 어저스트 만년필'},
+  'pilot-decimo':{src:'https://blueblack.co.kr/web/product/small/202606/8c5c16bf5ec1a3b54ec455e71e917c59.jpg',source:'https://blueblack.co.kr/web/product/small/202606/8c5c16bf5ec1a3b54ec455e71e917c59.jpg',alt:'파이롯트 캡리스 데시모 만년필'},
+  'faber-ambition-walnut':{src:'https://blueblack.co.kr/web/product/small/202604/2788c70660a5ba7e65b57e41f82d4a2f.jpg',source:'https://blueblack.co.kr/web/product/small/202604/2788c70660a5ba7e65b57e41f82d4a2f.jpg',alt:'파버카스텔 엠비션 만년필 월넛우드'},
+  'pilot-custom-urushi-blue':{src:'https://blueblack.co.kr/web/product/small/202605/cc2bc1a47b6d6d37c633844427cf305c.jpg',source:'https://blueblack.co.kr/web/product/small/202605/cc2bc1a47b6d6d37c633844427cf305c.jpg',alt:'파이롯트 커스텀 우루시 만년필 블루'}
+};
