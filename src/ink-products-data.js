d106127a4a9a9d915229cf6c4de33c5f36d6d24f
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-06-29 18:34:20 +0900

    Add verified ink products and decant prices

---
+++ src/ink-products-data.js
@@ -0,0 +1,77 @@
+export const INK_PRODUCTS = [
+  {id:'3oysters-delicious-hunters',brandKo:'3오이스터스',brandEn:'3 Oysters',productKo:'딜리셔스·헌터스',productEn:'Delicious / Hunters',price5:4100,price10:6700,color:'#315d7d',keywords:['쓰리오이스터스']},
+  {id:'3oysters-i-seoul-u',brandKo:'3오이스터스',brandEn:'3 Oysters',productKo:'아이서울유',productEn:'I Seoul U',price5:4600,price10:7700,color:'#cb4f58',keywords:['쓰리오이스터스']},
+
+  {id:'iwi-standard',brandKo:'IWI',brandEn:'IWI',productKo:'스탠다드',productEn:'Standard',price5:5600,price10:9700,color:'#405777',keywords:['아이더블유아이']},
+  {id:'iwi-pigment',brandKo:'IWI',brandEn:'IWI',productKo:'피그먼트',productEn:'Pigment',price5:6200,price10:10900,color:'#342f38',keywords:['아이더블유아이','안료']},
+  {id:'iwi-kasane',brandKo:'IWI',brandEn:'IWI',productKo:'카사네',productEn:'Kasane',price5:12500,price10:23500,color:'#7d3659',keywords:['아이더블유아이']},
+
+  {id:'graf-standard',brandKo:'그라폰 파버카스텔',brandEn:'Graf von Faber-Castell',productKo:'스탠다드',productEn:'Standard',price5:4800,price10:8100,color:'#3f704d',keywords:['그라폰','graf von']},
+  {id:'noodlers-22',brandKo:'누들러스',brandEn:"Noodler's",productKo:'22ml 제품',productEn:'22 ml bottle',price5:2800,price10:4100,color:'#5d3d76',keywords:['누들러','noodlers']},
+  {id:'noodlers-32',brandKo:'누들러스',brandEn:"Noodler's",productKo:'32ml 제품',productEn:'32 ml bottle',price5:3100,price10:4700,color:'#273f66',keywords:['누들러','noodlers']},
+
+  {id:'dominant-standard',brandKo:'도미넌트 인더스트리',brandEn:'Dominant Industry',productKo:'스탠다드',productEn:'Standard',price5:4300,price10:7100,color:'#274b68',keywords:['도미넌트']},
+  {id:'dominant-pearl',brandKo:'도미넌트 인더스트리',brandEn:'Dominant Industry',productKo:'펄',productEn:'Pearl',price5:4900,price10:8300,color:'#70567e',keywords:['도미넌트','쉬머']},
+  {id:'dominant-calligraphy',brandKo:'도미넌트 인더스트리',brandEn:'Dominant Industry',productKo:'캘리그라피·센티드',productEn:'Calligraphy / Scented',price5:5300,price10:9100,color:'#8b4c3f',keywords:['도미넌트','향']},
+
+  {id:'diamine-standard',brandKo:'디아민',brandEn:'Diamine',productKo:'스탠다드',productEn:'Standard',price5:3800,price10:6100,color:'#294d7c',keywords:['다이아민']},
+  {id:'diamine-shimmering',brandKo:'디아민',brandEn:'Diamine',productKo:'쉬머링',productEn:'Shimmering',price5:4600,price10:7700,color:'#73558a',keywords:['다이아민','쉬머']},
+  {id:'diamine-150',brandKo:'디아민',brandEn:'Diamine',productKo:'150주년',productEn:'150th Anniversary',price5:4750,price10:8000,color:'#7c393d',keywords:['다이아민','애니버서리']},
+  {id:'diamine-30ml',brandKo:'디아민',brandEn:'Diamine',productKo:'30ml 제품',productEn:'30 ml bottle',price5:3300,price10:5100,color:'#3e6d61',keywords:['다이아민']},
+
+  {id:'wearingeul-standard',brandKo:'글입다',brandEn:'Wearingeul',productKo:'스탠다드',productEn:'Standard',price5:4100,price10:6700,color:'#3e496b',keywords:['웨어링글','글입다공방']},
+  {id:'wearingeul-scented',brandKo:'글입다',brandEn:'Wearingeul',productKo:'센티드',productEn:'Scented',price5:4400,price10:7300,color:'#7b5145',keywords:['웨어링글','향']},
+  {id:'wearingeul-shimmer',brandKo:'글입다',brandEn:'Wearingeul',productKo:'쉬머·익스트림',productEn:'Shimmer / Extreme',price5:5000,price10:8500,color:'#6f4b85',keywords:['웨어링글','쉬머']},
+  {id:'wearingeul-chameleon',brandKo:'글입다',brandEn:'Wearingeul',productKo:'카멜레온·스타브라이트',productEn:'Chameleon / Star Bright',price5:5300,price10:9100,color:'#926d32',keywords:['웨어링글','스타브라이트']},
+
+  {id:'montegrappa-standard',brandKo:'몬테그라파',brandEn:'Montegrappa',productKo:'스탠다드',productEn:'Standard',price5:4000,price10:5700,color:'#3e5571',keywords:['몬테그라빠']},
+  {id:'colorverse-greek',brandKo:'칼라버스',brandEn:'Colorverse',productKo:'그리스 신화',productEn:'Greek Mythology',price5:4500,price10:7500,color:'#2d6683',keywords:['컬러버스']},
+  {id:'colorverse-indigo',brandKo:'칼라버스',brandEn:'Colorverse',productKo:'인디고',productEn:'Indigo',price5:4800,price10:8100,color:'#273b72',keywords:['컬러버스']},
+  {id:'colorverse-magic-fortune',brandKo:'칼라버스',brandEn:'Colorverse',productKo:'매직 포춘',productEn:'Magic Fortune',price5:5300,price10:9100,color:'#70487c',keywords:['컬러버스']},
+  {id:'ink-institute-taiwan-tea',brandKo:'잉크 인스티튜트',brandEn:'Ink Institute',productKo:'타이완 티',productEn:'Taiwan Tea',price5:5200,price10:8900,color:'#70513a',keywords:['잉크인스티튜트']},
+  {id:'robert-oster-standard',brandKo:'로버트 오스터',brandEn:'Robert Oster',productKo:'스탠다드',productEn:'Standard',price5:4500,price10:7500,color:'#2f6572',keywords:['로버트오스터']},
+  {id:'robert-oster-shake',brandKo:'로버트 오스터',brandEn:'Robert Oster',productKo:'쉐이크 앤 쉬머',productEn:"Shake 'N' Shimmy",price5:5300,price10:9100,color:'#745b83',keywords:['로버트오스터','쉬머']},
+
+  {id:'monteverde-standard',brandKo:'몬테베르데',brandEn:'Monteverde',productKo:'스탠다드',productEn:'Standard',price5:4500,price10:7500,color:'#376852',keywords:[]},
+  {id:'visconti-standard',brandKo:'비스콘티',brandEn:'Visconti',productKo:'스탠다드',productEn:'Standard',price5:3700,price10:5900,color:'#3e5470',keywords:[]},
+  {id:'visconti-van-gogh',brandKo:'비스콘티',brandEn:'Visconti',productKo:'반고흐',productEn:'Van Gogh',price5:5000,price10:8500,color:'#4c6f83',keywords:['고흐']},
+
+  {id:'sailor-standard',brandKo:'세일러',brandEn:'Sailor',productKo:'스탠다드',productEn:'Standard',price5:5700,price10:9900,color:'#284d77',keywords:[]},
+  {id:'sailor-manyo',brandKo:'세일러',brandEn:'Sailor',productKo:'만요',productEn:'Manyo',price5:5100,price10:8700,color:'#526c51',keywords:['만엽']},
+  {id:'sailor-studio',brandKo:'세일러',brandEn:'Sailor',productKo:'잉크 스튜디오',productEn:'Ink Studio',price5:7200,price10:12900,color:'#515271',keywords:['스튜디오']},
+  {id:'sailor-shikiori-shimmer',brandKo:'세일러',brandEn:'Sailor',productKo:'시키오리 쉬머',productEn:'Shikiori Shimmer',price5:6500,price10:11500,color:'#825374',keywords:['사계직','시키오리']},
+  {id:'sailor-gentle',brandKo:'세일러',brandEn:'Sailor',productKo:'신형 젠틀',productEn:'New Gentle',price5:3700,price10:5900,color:'#305b76',keywords:['젠틀']},
+  {id:'sailor-yurameku',brandKo:'세일러',brandEn:'Sailor',productKo:'유라메쿠',productEn:'Yurameku',price5:5000,price10:8500,color:'#6a637f',keywords:[]},
+  {id:'sailor-kingdom-note',brandKo:'세일러',brandEn:'Sailor',productKo:'킹덤노트',productEn:'Kingdom Note',price5:4500,price10:7500,color:'#4c634f',keywords:[]},
+
+  {id:'super5-standard',brandKo:'수퍼5',brandEn:'Super5',productKo:'스탠다드',productEn:'Standard',price5:4300,price10:7100,color:'#346b74',keywords:['슈퍼5']},
+  {id:'sheaffer-standard',brandKo:'쉐퍼',brandEn:'Sheaffer',productKo:'스탠다드',productEn:'Standard',price5:4600,price10:7700,color:'#3f4e68',keywords:['셰퍼']},
+  {id:'aurora-standard',brandKo:'오로라',brandEn:'Aurora',productKo:'스탠다드',productEn:'Standard',price5:4500,price10:7500,color:'#314c72',keywords:[]},
+  {id:'aurora-100',brandKo:'오로라',brandEn:'Aurora',productKo:'100주년',productEn:'100th Anniversary',price5:4700,price10:7900,color:'#783c48',keywords:['애니버서리']},
+  {id:'waterman-standard',brandKo:'워터맨',brandEn:'Waterman',productKo:'스탠다드',productEn:'Standard',price5:4000,price10:6500,color:'#335c78',keywords:[]},
+  {id:'ink-institute-standard',brandKo:'잉크 인스티튜트',brandEn:'Ink Institute',productKo:'스탠다드',productEn:'Standard',price5:5700,price10:9900,color:'#4f6650',keywords:['잉크인스티튜트']},
+  {id:'jacques-herbin-scented',brandKo:'쟈크 에르뱅',brandEn:'Jacques Herbin',productKo:'센티드',productEn:'Scented',price5:6600,price10:11700,color:'#754b45',keywords:['자크허빈','쟈크허빈','향']},
+  {id:'jacques-herbin-collections',brandKo:'쟈크 에르뱅',brandEn:'Jacques Herbin',productKo:'1670·1798·350',productEn:'1670 / 1798 / 350',price5:5600,price10:9700,color:'#6e4a7b',keywords:['자크허빈','쟈크허빈']},
+  {id:'jacques-herbin-artist',brandKo:'쟈크 에르뱅',brandEn:'Jacques Herbin',productKo:'크리에이션 아티스트',productEn:'Creation Artists',price5:6000,price10:10500,color:'#8a6137',keywords:['자크허빈','쟈크허빈']},
+  {id:'j-herbin-standard',brandKo:'제이 에르뱅',brandEn:'J. Herbin',productKo:'스탠다드',productEn:'Standard',price5:4200,price10:6900,color:'#426b62',keywords:['제이허빈','허빈']},
+
+  {id:'kaweco-standard',brandKo:'카웨코',brandEn:'Kaweco',productKo:'스탠다드',productEn:'Standard',price5:4000,price10:6500,color:'#415d72',keywords:[]},
+  {id:'kakimori-pigment',brandKo:'카키모리',brandEn:'Kakimori',productKo:'피그먼트',productEn:'Pigment',price5:6600,price10:11700,color:'#3f6a54',keywords:['안료']},
+  {id:'conklin-60',brandKo:'콘클린',brandEn:'Conklin',productKo:'60ml 제품',productEn:'60 ml bottle',price5:3600,price10:5700,color:'#4a5870',keywords:[]},
+  {id:'conklin-90',brandKo:'콘클린',brandEn:'Conklin',productKo:'90ml 제품',productEn:'90 ml bottle',price5:3200,price10:4900,color:'#6d4a59',keywords:[]},
+  {id:'faber-standard',brandKo:'파버카스텔',brandEn:'Faber-Castell',productKo:'스탠다드',productEn:'Standard',price5:3000,price10:4500,color:'#376549',keywords:['파버 카스텔']},
+  {id:'pilot-standard',brandKo:'파이롯트',brandEn:'Pilot',productKo:'스탠다드',productEn:'Standard',price5:2500,price10:3500,color:'#315b77',keywords:['파일럿']},
+  {id:'pilot-iroshizuku',brandKo:'파이롯트',brandEn:'Pilot',productKo:'이로시주쿠',productEn:'Iroshizuku',price5:4300,price10:7100,color:'#5a547d',keywords:['파일럿','이로시즈쿠']},
+  {id:'parker-quink',brandKo:'파카',brandEn:'Parker',productKo:'큉크',productEn:'Quink',price5:3200,price10:4900,color:'#2f4e70',keywords:[]},
+  {id:'fwp-standard',brandKo:'페리스 휠 프레스',brandEn:'Ferris Wheel Press',productKo:'스탠다드',productEn:'Standard',price5:5700,price10:9900,color:'#6a4d75',keywords:['페리스휠프레스','fwp']},
+  {id:'fwp-shimmer',brandKo:'페리스 휠 프레스',brandEn:'Ferris Wheel Press',productKo:'쉬머',productEn:'Shimmer',price5:6700,price10:11900,color:'#8b6736',keywords:['페리스휠프레스','fwp','펄']},
+  {id:'pelikan-4001',brandKo:'펠리칸',brandEn:'Pelikan',productKo:'4001',productEn:'4001',price5:2300,price10:3100,color:'#2f5875',keywords:[]},
+  {id:'pelikan-edelstein',brandKo:'펠리칸',brandEn:'Pelikan',productKo:'에델슈타인',productEn:'Edelstein',price5:4500,price10:7500,color:'#36675b',keywords:[]},
+  {id:'platinum-standard',brandKo:'플래티넘',brandEn:'Platinum',productKo:'스탠다드',productEn:'Standard',price5:4000,price10:6500,color:'#3a526f',keywords:[]},
+  {id:'platinum-carbon',brandKo:'플래티넘',brandEn:'Platinum',productKo:'카본',productEn:'Carbon',price5:4500,price10:7500,color:'#2f3034',keywords:['안료']},
+  {id:'platinum-classic',brandKo:'플래티넘',brandEn:'Platinum',productKo:'클래식',productEn:'Classic',price5:4600,price10:7700,color:'#704d3d',keywords:[]}
+];
+
+export function formatWon(value){
+  return new Intl.NumberFormat('ko-KR').format(value)+'원';
+}
