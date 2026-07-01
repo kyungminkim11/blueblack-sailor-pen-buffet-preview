1f82e8b56506deac54491550983fbc859bb471ca
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-07-01 20:05:43 +0900

    Keep only uploaded OMAS decant ink colors

--- src/ink-inventory-colors.js
+++ src/ink-inventory-colors.js
@@ -1,14 +1,8 @@
 export const INK_INVENTORY_COLORS = [
-  {id:'omas-bottle-violet',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'바이올렛',nameEn:'Violet',nameJa:'バイオレット',nameZhHans:'紫罗兰',nameZhHant:'紫羅蘭',hex:'#6F4A8E',itemCode:'OMO00E000800-00'},
-  {id:'omas-bottle-green',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'그린',nameEn:'Green',nameJa:'グリーン',nameZhHans:'绿色',nameZhHant:'綠色',hex:'#3F704D',itemCode:'OMO00E000900-00'},
-  {id:'omas-bottle-sepia',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'세피아',nameEn:'Sepia',nameJa:'セピア',nameZhHans:'棕褐色',nameZhHant:'棕褐色',hex:'#704A35',itemCode:'OMO00E001100-00'},
-  {id:'omas-bottle-red',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'레드',nameEn:'Red',nameJa:'レッド',nameZhHans:'红色',nameZhHant:'紅色',hex:'#A84045',itemCode:'OMO00E002100-00'},
-  {id:'omas-bottle-turquoise',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'터키옥',nameEn:'Turquoise',nameJa:'ターコイズ',nameZhHans:'绿松石色',nameZhHant:'綠松石色',hex:'#2F8D91',itemCode:'OMO00E002200-00'},
-  {id:'omas-bottle-grey',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'그레이',nameEn:'Grey',nameJa:'グレー',nameZhHans:'灰色',nameZhHant:'灰色',hex:'#626B74',itemCode:'OMO00E002300-00'},
-  {id:'omas-cartridge-black',brandKo:'오마스',brandEn:'OMAS',form:'cartridge',volume:null,nameKo:'블랙',nameEn:'Black',nameJa:'ブラック',nameZhHans:'黑色',nameZhHant:'黑色',hex:'#20242A',itemCode:'OMO00G003700-00'},
-  {id:'omas-cartridge-green',brandKo:'오마스',brandEn:'OMAS',form:'cartridge',volume:null,nameKo:'그린',nameEn:'Green',nameJa:'グリーン',nameZhHans:'绿色',nameZhHant:'綠色',hex:'#3F704D',itemCode:'OMO00G003800-00'},
-  {id:'omas-cartridge-red',brandKo:'오마스',brandEn:'OMAS',form:'cartridge',volume:null,nameKo:'레드',nameEn:'Red',nameJa:'レッド',nameZhHans:'红色',nameZhHant:'紅色',hex:'#A84045',itemCode:'OMO00G003900-00'},
-  {id:'omas-cartridge-blue',brandKo:'오마스',brandEn:'OMAS',form:'cartridge',volume:null,nameKo:'블루',nameEn:'Blue',nameJa:'ブルー',nameZhHans:'蓝色',nameZhHant:'藍色',hex:'#315E91',itemCode:'OMO00G004000-00'},
-  {id:'omas-cartridge-turquoise',brandKo:'오마스',brandEn:'OMAS',form:'cartridge',volume:null,nameKo:'터키옥',nameEn:'Turquoise',nameJa:'ターコイズ',nameZhHans:'绿松石色',nameZhHant:'綠松石色',hex:'#2F8D91',itemCode:'OMO00G004100-00'},
-  {id:'omas-cartridge-orange',brandKo:'오마스',brandEn:'OMAS',form:'cartridge',volume:null,nameKo:'오렌지',nameEn:'Orange',nameJa:'オレンジ',nameZhHans:'橙色',nameZhHant:'橙色',hex:'#D06A2D',itemCode:'OMO00G004200-00'}
+  {id:'omas-bottle-violet',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'바이올렛',nameEn:'Violet',nameJa:'バイオレット',nameZhHans:'紫罗兰',nameZhHant:'紫羅蘭',hex:'#6F4A8E',itemCode:'OMO00E000800-00',productTitle:'오마스 병잉크 75ml 바이올렛',source:'uploaded-sales-list'},
+  {id:'omas-bottle-green',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'그린',nameEn:'Green',nameJa:'グリーン',nameZhHans:'绿色',nameZhHant:'綠色',hex:'#3F704D',itemCode:'OMO00E000900-00',productTitle:'오마스 병잉크 75ml 그린',source:'uploaded-sales-list'},
+  {id:'omas-bottle-sepia',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'세피아',nameEn:'Sepia',nameJa:'セピア',nameZhHans:'棕褐色',nameZhHant:'棕褐色',hex:'#704A35',itemCode:'OMO00E001100-00',productTitle:'오마스 병잉크 75ml 세피아',source:'uploaded-sales-list'},
+  {id:'omas-bottle-red',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'레드',nameEn:'Red',nameJa:'レッド',nameZhHans:'红色',nameZhHant:'紅色',hex:'#A84045',itemCode:'OMO00E002100-00',productTitle:'오마스 병잉크 75ml 레드',source:'uploaded-sales-list'},
+  {id:'omas-bottle-turquoise',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'터키옥',nameEn:'Turquoise',nameJa:'ターコイズ',nameZhHans:'绿松石色',nameZhHant:'綠松石色',hex:'#2F8D91',itemCode:'OMO00E002200-00',productTitle:'오마스 병잉크 75ml 터키옥',source:'uploaded-sales-list'},
+  {id:'omas-bottle-grey',brandKo:'오마스',brandEn:'OMAS',form:'bottle',volume:'75ml',nameKo:'그레이',nameEn:'Grey',nameJa:'グレー',nameZhHans:'灰色',nameZhHant:'灰色',hex:'#626B74',itemCode:'OMO00E002300-00',productTitle:'오마스 병잉크 75ml 그레이',source:'uploaded-sales-list'}
 ];
