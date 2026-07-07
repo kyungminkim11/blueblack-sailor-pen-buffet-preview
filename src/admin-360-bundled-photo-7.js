a465666fdd1bb8bd39fb9d2297153144ce9ea076
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-07-07 10:51:11 +0900

    Assemble 1F roadview panorama 7

---
+++ src/admin-360-bundled-photo-7.js
@@ -0,0 +1,2 @@
+import {p7c1,p7c2,p7c3,p7c4} from './admin-360-photo7-chunks.js';
+export const bundledPhoto7={spotId:'f1-07',floor:1,name:'blueblack-1f-07-left-upper-corner.webp',type:'image/webp',width:240,height:120,size:2482,updatedAt:'2026-07-07T09:00:00+09:00',dataUrl:`data:image/webp;base64,${p7c1}${p7c2}${p7c3}${p7c4}`};
